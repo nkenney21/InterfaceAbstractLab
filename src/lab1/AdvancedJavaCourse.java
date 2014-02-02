3c46b3b92ff66a2bf6b3942ca21c3c50fc1ef648
neilkenney <neilkenney@192.168.0.104>
2014-02-02 10:11:14 -0600

    Interface Abstract Lab added Abstract Super Class

--- src/lab1/AdvancedJavaCourse.java
+++ src/lab1/AdvancedJavaCourse.java
@@ -8,10 +8,8 @@ import javax.swing.JOptionPane;
  * @author      your name goes here
  * @version     1.00
  */
-public class AdvancedJavaCourse {
-    String courseName;
-    private String courseNumber;
-    private double credits;
+public class AdvancedJavaCourse extends CourseStructure{
+    
     private String prerequisites;
 
     public AdvancedJavaCourse(String courseName, String courseNumber) {
@@ -37,6 +35,11 @@ public class AdvancedJavaCourse {
         this.prerequisites = prerequisites;
     }
 
+    /**
+     *
+     * @param credits
+     */
+    @Override
     public void setCredits(double credits) {
         if(credits < 0.5 || credits > 4.0) {
             JOptionPane.showMessageDialog(null,
@@ -46,20 +49,6 @@ public class AdvancedJavaCourse {
         this.setCredits(credits);
     }
 
-    public String getCourseName() {
-        return courseName;
-    }
-
-    public void setCourseName(String courseName) {
-        this.courseName = courseName;
-    }
-
-    public String getCourseNumber() {
-        return courseNumber;
-    }
-
-    public void setCourseNumber(String courseNumber) {
-        this.courseNumber = courseNumber;
-    }
+   
     
 }
