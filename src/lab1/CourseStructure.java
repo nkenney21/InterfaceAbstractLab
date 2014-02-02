c31e67c7c173dad54fb3b25a471c3406c6509336
neilkenney <neilkenney@192.168.0.104>
2014-02-02 10:11:55 -0600

    Interface Abstract Lab fixed AdvancedJavaCourse Super Class

--- src/lab1/CourseStructure.java
+++ src/lab1/CourseStructure.java
@@ -10,6 +10,35 @@ package lab1;
  *
  * @author neilkenney
  */
-public class CourseStructure {
+public abstract  class CourseStructure {
+    
+    private String courseName;
+    private String courseNumber;
+    private double credits;
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseNumber() {
+        return courseNumber;
+    }
+
+    public void setCourseNumber(String courseNumber) {
+        this.courseNumber = courseNumber;
+    }
+
+    public double getCredits() {
+        return credits;
+    }
+
+    public void setCredits(double credits) {
+        this.credits = credits;
+    }
+    
     
 }
