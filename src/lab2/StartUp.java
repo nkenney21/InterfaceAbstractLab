72174f91cc379636326f9bf80159a036b6389c34
neilkenney <neilkenney@192.168.0.104>
2014-02-02 18:11:58 -0600

    Lab 2 StartUp Finished1

--- src/lab2/StartUp.java
+++ src/lab2/StartUp.java
@@ -5,6 +5,9 @@ package lab2;
 
 public class StartUp {
     public static void main(String[] args) {
+        /* I like using an interface because I can controll exactly how I want all of my classes to look
+        I have found it easier to use and understand more than using abstract methods.
+        */
         
         IntroToProgrammingCourse ipc1 = new IntroToProgrammingCourse("Intro To Programming", "0012", 3);
         IntroJavaCourse ijc1 = new IntroJavaCourse("Intro to Java", "0013", 4);
